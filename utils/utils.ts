5fa93ede4afa6b8f858f3e954f933fd37e0eb16f
Fabio Rotondo <fabio.rotondo@gmail.com>
2024-03-06 09:37:04 +0100

    Add debounce function to utils.ts

--- utils/utils.ts
+++ utils/utils.ts
@@ -264,3 +264,20 @@ export const intToRGBHex = ( rgb: number[] ) => {
 
 	return `#${ r }${ g }${ b }`;
 };
+
+
+export function debounce<T extends ( ...args: any[] ) => any> (
+	func: T,
+	delay: number
+): ( ...args: Parameters<T> ) => void {
+	let timeoutId: ReturnType<typeof setTimeout> | null = null;
+
+	return ( ...args: Parameters<T> ): void => {
+		if ( timeoutId ) {
+			clearTimeout( timeoutId );
+		}
+		timeoutId = setTimeout( () => {
+			func( ...args );
+		}, delay );
+	};
+}
