911e5cde122566cf46638be86c8f3410cab0ee43
pigraphic <developing@pig-fx.it>
2024-03-15 18:34:51 +0100

    Update button color variable, added LayoutVars type

--- types/types.ts
+++ types/types.ts
@@ -2,6 +2,7 @@ export type Size = 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
 
 // export type Color = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' | 'alternative';
 export type Color = 'mode1' | 'mode2' | 'mode3' | 'mode4' | 'primary' | 'secondary' | 'success' | 'error' | 'danger' | 'warning' | 'info' | 'light' | 'dark' | 'alternative' | 'background' | 'color' | 'link';
+export type LayoutVars = 'font-size' | 'font-weight' | 'line-height' | 'border-radius' | 'border-width' | 'border-style' | 'button-padding-y' | 'button-padding-x' | 'form-padding-y' | 'form-padding-x';
 
 export type Variant = 'solid' | 'outline' | 'link';
 export type InputVariant = 'material' | 'boxed' | 'plain' | 'none';
