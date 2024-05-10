ff4f296c0d025a5ea4854d64f001b87dc449a1a9
Fabio Rotondo <fabio.rotondo@gmail.com>
2024-05-10 06:50:47 +0200

    ported to svelte5

--- components/Paginator.svelte
+++ components/Paginator.svelte
@@ -1,37 +1,32 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
-
-	const dispatch = createEventDispatcher();
-
 	import Button from './Button.svelte';
 	import type { Color } from '$liwe3/types/types';
 
-	export let mode: Color = 'mode1';
+	interface PaginatorProps {
+		mode?: Color;
+		page?: number; // current page
+		rows?: number; // rows per page
+		total?: number; // total number of rows
 
-	// current page
-	export let page = 1;
-	// rows per page
-	export let rows = 10;
-	// total number of rows
-	export let total = 0;
+		pagechange?: (page: number, rows: number) => void;
+	}
 
-	// pagination options
-	// export let options: number[] = [10, 20, 50, 100];
+	let { mode = 'mode1', page = 1, rows = 10, total = 0, pagechange }: PaginatorProps = $props();
 
-	export const setPage = (p: number) => {
-		page = p;
+	let pages = $derived(Math.ceil(total / rows));
+
+	const setPage = (num: number) => {
+		page = num;
+		pagechange && pagechange(page, rows);
 	};
-
-	$: pages = Math.ceil(total / rows);
-	$: dispatch('pagechange', { page, rows });
 </script>
 
 <div class="paginator">
-	<Button {mode} size="sm" disabled={page == 1} on:click={() => (page = 1)}>First</Button>
-	<Button {mode} size="sm" disabled={page == 1} on:click={() => (page -= 1)}>Prev</Button>
+	<Button {mode} size="sm" disabled={page == 1} onclick={() => setPage(1)}>First</Button>
+	<Button {mode} size="sm" disabled={page == 1} onclick={() => setPage(page - 1)}>Prev</Button>
 	{page} of {pages}
-	<Button {mode} size="sm" disabled={page == pages} on:click={() => (page += 1)}>Next</Button>
-	<Button {mode} size="sm" disabled={page == pages} on:click={() => (page = pages)}>Last</Button>
+	<Button {mode} size="sm" disabled={page == pages} onclick={() => setPage(page + 1)}>Next</Button>
+	<Button {mode} size="sm" disabled={page == pages} onclick={() => setPage(pages)}>Last</Button>
 </div>
 
 <style>
