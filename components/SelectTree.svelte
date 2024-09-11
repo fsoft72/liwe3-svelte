9766bb0b4194a41a46f3f8fd566f8d2cbfa83275
Fabio Rotondo <fabio.rotondo@gmail.com>
2024-09-11 10:52:09 +0200

    Completely updated to support Tree structure

--- components/SelectTree.svelte
+++ components/SelectTree.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import type { TreeItem } from '$liwe3/utils/tree';
+	import type { Tree } from '$liwe3/utils/tree';
 	import type { Color } from '$liwe3/types/types';
 
 	interface SelectTreeProps {
+		tree: Tree;
 		name?: string;
-		tree: TreeItem[];
 		level?: number;
 		fontSize?: string;
 		addEmpty?: boolean;
@@ -15,8 +15,8 @@
 	}
 
 	let {
+		tree,
 		name = '',
-		tree = [],
 		level = 0,
 		fontSize = '1.2em',
 		addEmpty = true,
@@ -25,37 +25,41 @@
 		onchange
 	}: SelectTreeProps = $props();
 
-	let spaces = $derived('&nbsp;'.repeat(level * 4));
-
 	const onChange = (e: Event) => {
 		const select = e.target as HTMLSelectElement;
 		const id = select.value;
 
 		onchange && onchange(id);
 	};
+
+	// recursively create the options list with these fields: id, name, spaces
+	const _createOptions = (options: any[], tree: Tree, level: number = 0) => {
+		tree.children.map((item) => {
+			options.push({
+				id: item.id,
+				name: item.name,
+				level,
+				spaces: '&nbsp;'.repeat(level * 4)
+			});
+
+			if (item.children) {
+				_createOptions(options, item as Tree, level + 1);
+			}
+		});
+
+		return options;
+	};
 </script>
 
 <div class={mode}>
-	{#if level == 0}
-		<select {name} style={`font-size: ${fontSize}`} onchange={onChange}>
-			{#if addEmpty}
-				<option value="">-</option>
-			{/if}
-			{#each tree as item (item.id)}
-				<option value={item.id} selected={item.id == value}>{item.name}</option>
-				{#if item.children && item.children.length}
-					<svelte:self tree={item.children} level={level + 1} on:change {value} />
-				{/if}
-			{/each}
-		</select>
-	{:else}
-		{#each tree as item (item.id)}
-			<option value={item.id} selected={item.id == value}>{@html spaces}{item.name}</option>
-			{#if item.children && item.children.length}
-				<svelte:self tree={item.children} level={level + 1} on:change {value} />
-			{/if}
+	<select {name} style:font-size={fontSize} onchange={onChange}>
+		{#if addEmpty}
+			<option value="">(Select)</option>
+		{/if}
+		{#each _createOptions([], tree) as item (item.id)}
+			<option value={item.id} selected={item.id == value}>{@html item.spaces}{item.name}</option>
 		{/each}
-	{/if}
+	</select>
 </div>
 
 <style>
