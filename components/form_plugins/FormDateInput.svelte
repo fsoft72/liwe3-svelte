cd606469b0487ff11fc00186f392255be4daa14b
pigraphic <developing@pig-fx.it>
2024-11-06 12:59:31 +0100

    ADD: create FormDateInput component for date and datetime input handling

---
+++ components/form_plugins/FormDateInput.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Input from '$liwe3/components/Input.svelte';
+	import type { FormField } from '../FormCreator.svelte';
+
+	interface Props {
+		field: FormField;
+
+		name: string;
+
+        time?: boolean;
+
+		// dependency injection
+		_v: (field: FormField) => any;
+
+		// events
+		onchange: (name: string, value: any, field: FormField) => void;
+
+		// extra
+		[key: string]: any;
+	}
+
+	let { field, name, time, onchange, _v, ...props }: Props = $props();
+</script>
+
+<Input
+	{...field}
+	{...field?.extra ?? {}}
+	{...props}
+	{name}
+	value={_v(field)}
+	type={time ? 'datetime-local' : 'date'}
+	onchange={(e: any) => onchange(name, e, field)}
+/>
