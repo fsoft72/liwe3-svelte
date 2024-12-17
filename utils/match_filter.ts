648cac33e3cc3991fadfa117904603abf3a7c580
Fabio Rotondo <fabio.rotondo@gmail.com>
2024-12-17 12:24:13 +0100

    REFINE: update filterModes enum for improved readability and consistency in matchFilter function

--- utils/match_filter.ts
+++ utils/match_filter.ts
@@ -1,20 +1,19 @@
 export enum filterModes {
-	'==',
-	'!=',
-	'>',
-	'<',
-	'>=',
-	'<=',
-	'n>',
-	'n<',
-	'n>=',
-	'n<=',
-	'contains',
-	'not_contains',
-	'starts_with',
-	'ends_with'
+	EQUAL = 'equal',
+	NOT_EQUAL = 'not_equal',
+	GREATER_THAN = 'greater_than',
+	LESS_THAN = 'less_than',
+	GREATER_THAN_OR_EQUAL = 'greater_than_or_equal',
+	LESS_THAN_OR_EQUAL = 'less_than_or_equal',
+	NUMERIC_GREATER_THAN = 'numeric_greater_than',
+	NUMERIC_LESS_THAN = 'numeric_less_than',
+	NUMERIC_GREATER_THAN_OR_EQUAL = 'numeric_greater_than_or_equal',
+	NUMERIC_LESS_THAN_OR_EQUAL = 'numeric_less_than_or_equal',
+	CONTAINS = 'contains',
+	NOT_CONTAINS = 'not_contains',
+	STARTS_WITH = 'starts_with',
+	ENDS_WITH = 'ends_with'
 }
-
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const matchFilter = ( obj: Record<string, any>, name: string, value: string, mode: filterModes ) => {
@@ -23,20 +22,19 @@ export const matchFilter = ( obj: Record<string, any>, name: string, value: stri
 	if ( fld === undefined ) return false;
 
 	switch ( mode ) {
-		case filterModes[ '==' ]: return fld == value;
-		case filterModes.contains: return fld.toLowerCase().includes( value.toLowerCase() );
-		case filterModes[ '<' ]: return fld < value;
-		case filterModes[ '<=' ]: return fld <= value;
-		case filterModes[ '>' ]: return fld > value;
-		case filterModes[ '>=' ]: return fld >= value;
-		case filterModes[ 'n<' ]: return parseFloat( fld ) < parseFloat( value );
-		case filterModes[ 'n<=' ]: return parseFloat( fld ) <= parseFloat( value );
-		case filterModes[ 'n>' ]: return parseFloat( fld ) > parseFloat( value );
-		case filterModes[ 'n>=' ]: return parseFloat( fld ) >= parseFloat( value );
-		case filterModes[ '!=' ]: return fld != value;
-		case filterModes.starts_with: return fld.toLowerCase().startsWith( value.toLowerCase() );
-		case filterModes.ends_with: return fld.toLowerCase().endsWith( value.toLowerCase() );
-
+		case filterModes.EQUAL: return fld == value;
+		case filterModes.CONTAINS: return fld.toLowerCase().includes( value.toLowerCase() );
+		case filterModes.LESS_THAN: return fld < value;
+		case filterModes.LESS_THAN_OR_EQUAL: return fld <= value;
+		case filterModes.GREATER_THAN: return fld > value;
+		case filterModes.GREATER_THAN_OR_EQUAL: return fld >= value;
+		case filterModes.NUMERIC_LESS_THAN: return parseFloat( fld ) < parseFloat( value );
+		case filterModes.NUMERIC_LESS_THAN_OR_EQUAL: return parseFloat( fld ) <= parseFloat( value );
+		case filterModes.NUMERIC_GREATER_THAN: return parseFloat( fld ) > parseFloat( value );
+		case filterModes.NUMERIC_GREATER_THAN_OR_EQUAL: return parseFloat( fld ) >= parseFloat( value );
+		case filterModes.NOT_EQUAL: return fld != value;
+		case filterModes.STARTS_WITH: return fld.toLowerCase().startsWith( value.toLowerCase() );
+		case filterModes.ENDS_WITH: return fld.toLowerCase().endsWith( value.toLowerCase() );
 
 		default:
 			return false;
