045a2d071787eeb65d9334a7636cce8a0df963f7
Fabio Rotondo <fabio.rotondo@gmail.com>
2024-12-23 19:20:01 +0100

    better CSS

--- components/DataGrid.svelte
+++ components/DataGrid.svelte
@@ -1059,16 +1059,16 @@
 	td.editable::before {
 		content: '';
 		position: absolute;
-		top: 1rem;
+		top: 0.2rem;
 		right: 0.2rem;
 		width: 0.7rem;
 		height: 0.7rem;
 		background-image: url('data:image/svg+xml;utf8,<svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"> <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"></path> </svg>');
 		background-size: contain;
 		background-repeat: no-repeat;
-		transform:
+		/*transform:
 			height 0.8s ease,
-			width 0.8s ease;
+			width 0.8s ease;*/
 	}
 
 	/*
@@ -1090,9 +1090,11 @@
 		*/
 	}
 
+	/*
 	table.large {
 		td.editable::before {
 			top: 2rem;
 		}
 	}
+		*/
 </style>
