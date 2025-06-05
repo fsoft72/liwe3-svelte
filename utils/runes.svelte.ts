738a966d52459bab57d710a77182e2849d6c7eb2
Fabio Rotondo <fabio.rotondo@gmail.com>
2025-06-05 17:52:32 +0200

    fix: ensure runeDebug function only runs in development mode

--- utils/runes.svelte.ts
+++ utils/runes.svelte.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { dev } from '$app/environment';
 
 /**
  * This function dumps debug messages to the console.
@@ -6,6 +7,9 @@
  * Every parameter is passed through the snapshot function to get a snapshot of the object.
  */
 export const runeDebug = ( ...args: any[] ) => {
+	if ( !dev ) return;
+
+	// only run in development mode
 	const snapshotArgs = args.map( arg => {
 		try {
 			return $state.snapshot( arg );
